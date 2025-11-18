60ed5c54d4c280904dc32499adb1ed49bb1f8a43
jasonbdt <ja.bladt@gmail.com>
2025-11-18 13:00:10 +0100

    db: Update name of user table

--- app/internal/models.py
+++ app/internal/models.py
@@ -2,5 +2,7 @@ from sqlmodel import Field, SQLModel
 
 
 class User(SQLModel, table=True):
+    __tablename__ = "users"
+
     id: int = Field(default=None, primary_key=True)
     avatarName: str = Field(index=True)
