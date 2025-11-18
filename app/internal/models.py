2f20ce5cd34efe9e97b70e40cd785f003d4e6c28
jasonbdt <ja.bladt@gmail.com>
2025-11-18 13:18:41 +0100

    db: Expand User model

--- app/internal/models.py
+++ app/internal/models.py
@@ -5,4 +5,6 @@ class User(SQLModel, table=True):
     __tablename__ = "users"
 
     id: int = Field(default=None, primary_key=True)
-    avatarName: str = Field(index=True)
+    avatarName: str = Field(index=True, nullable=False)
+    emailAddress: str = Field(unique=True, nullable=False)
+    password: str = Field(nullable=False)
