dca48253e685ce80f6a23382285475a960c38fbd
jasonbdt <ja.bladt@gmail.com>
2025-11-18 13:10:58 +0100

    feat: Add route to get details for specific user

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from contextlib import asynccontextmanager
 from typing import Annotated
 
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, HTTPException
 from sqlmodel import select
 
 from app.internal.db import create_db_and_tables, SessionDep
@@ -33,4 +33,17 @@ def get_all_users(
         "status": 200,
         "message": "Success",
         "users": users
+    }
+
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int, session: SessionDep):
+    user = session.get(User, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    return {
+        "status": 200,
+        "message": "Success",
+        "user": user
     }
