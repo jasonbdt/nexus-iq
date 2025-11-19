e8dc818ecda13fc6806c4dcfdfb0166905387f1a
jasonbdt <ja.bladt@gmail.com>
2025-11-19 05:26:35 +0100

    refactor: Move authentication routes into own package

--- app/main.py
+++ app/main.py
@@ -1,18 +1,15 @@
 from contextlib import asynccontextmanager
-from datetime import timedelta
 from typing import Annotated
 
-from fastapi import FastAPI, HTTPException, status, Query
+from fastapi import FastAPI, HTTPException, Query
 from fastapi.params import Depends
-from fastapi.security import OAuth2PasswordRequestForm
-from sqlalchemy.exc import IntegrityError
 from sqlmodel import select
 
 from app.internal.db import create_db_and_tables, SessionDep
-from app.internal.models import User, UserResponse, UserSignUpRequest
-from app.internal.auth import JWT_TOKEN_EXPIRE_MINUTES, Token, \
-    authenticate_user, get_password_hash, oauth2_scheme, create_access_token, \
-    get_current_active_user
+from app.internal.models import User, UserResponse
+from app.internal.auth import oauth2_scheme, get_current_active_user
+
+from .routers import auth
 
 
 @asynccontextmanager
@@ -21,6 +18,7 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
+app.include_router(auth.router)
 
 @app.get("/")
 def index():
@@ -28,25 +26,6 @@ def index():
         "status": 200,
         "message": "It work's!"
     }
-
-
-# TODO: Change to sign_up
-@app.post("/users/", response_model=UserResponse)
-def create_user(user_in: UserSignUpRequest, session: SessionDep):
-    if user_in.password != user_in.password_confirm:
-        raise HTTPException(status_code=400, detail="Passwords do not match")
-
-    user_in.password = get_password_hash(user_in.password)
-    new_user = User(**user_in.model_dump())
-
-    try:
-        session.add(new_user)
-        session.commit()
-        session.refresh(new_user)
-    except IntegrityError:
-        raise HTTPException(status_code=400, detail="User already exists")
-
-    return new_user
 
 
 # TODO: Add Role-Based Authorization Check
@@ -103,24 +82,3 @@ def delete_user(
         "status": 200,
         "message": "Success"
     }
-
-
-@app.post("/login")
-async def login(
-    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
-    session: SessionDep
-) -> Token:
-    user = authenticate_user(form_data.username, form_data.password, session)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"}
-        )
-
-    access_token_expires = timedelta(minutes=JWT_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.emailAddress}, expires_delta=access_token_expires
-    )
-
-    return Token(access_token=access_token, token_type="bearer")
---
+++ app/routers/auth.py
@@ -0,0 +1,55 @@
+from typing import Annotated
+from datetime import timedelta
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+
+from sqlalchemy.exc import IntegrityError
+
+from ..internal.auth import JWT_TOKEN_EXPIRE_MINUTES, Token, \
+    authenticate_user, create_access_token, get_password_hash
+from ..internal.db import SessionDep
+from ..internal.models import User, UserResponse, UserSignUpRequest
+
+router = APIRouter(
+    tags=["Authentication"]
+)
+
+
+@router.post("/register", response_model=UserResponse)
+def register(user_in: UserSignUpRequest, session: SessionDep):
+    if user_in.password != user_in.password_confirm:
+        raise HTTPException(status_code=400, detail="Passwords do not match")
+
+    user_in.password = get_password_hash(user_in.password)
+    new_user = User(**user_in.model_dump())
+
+    try:
+        session.add(new_user)
+        session.commit()
+        session.refresh(new_user)
+    except IntegrityError:
+        raise HTTPException(status_code=400, detail="User already exists")
+
+    return new_user
+
+
+@router.post("/login")
+async def login(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    session: SessionDep
+) -> Token:
+    user = authenticate_user(form_data.username, form_data.password, session)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+
+    access_token_expires = timedelta(minutes=JWT_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.emailAddress}, expires_delta=access_token_expires
+    )
+
+    return Token(access_token=access_token, token_type="bearer")
