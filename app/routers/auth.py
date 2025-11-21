2da8263e3c7f216a4fd2d328caf3feb1dec3601b
jasonbdt <ja.bladt@gmail.com>
2025-11-21 06:13:47 +0100

    refactor: Add application log statements

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -13,16 +13,21 @@ from ..dependencies import JWT_TOKEN_EXPIRE_MINUTES
 from ..internal.auth import Token, authenticate_user, create_access_token, \
     get_password_hash
 from ..internal.db import SessionDep
+from ..internal.logging import get_logger
 from ..internal.models import User, UserResponse, UserSignUpRequest
 
 router = APIRouter(
     tags=["Authentication"]
 )
 
+logger = get_logger(__name__)
+
 
 @router.post("/register", response_model=UserResponse)
 def register(user_in: UserSignUpRequest, session: SessionDep):
+    logger.info(f"Try to register User with email: {user_in.emailAddress}")
     if user_in.password != user_in.password_confirm:
+        logger.warning(f"Passwords for User[{user_in.emailAddress}] do not match")
         raise HTTPException(status_code=400, detail="Passwords do not match")
 
     user_in.password = get_password_hash(user_in.password)
@@ -33,8 +38,10 @@ def register(user_in: UserSignUpRequest, session: SessionDep):
         session.commit()
         session.refresh(new_user)
     except IntegrityError:
+        logger.warning(f"User[{user_in.emailAddress}] already exists")
         raise HTTPException(status_code=400, detail="User already exists")
 
+    logger.info(f"User[{new_user.id}] created successfully")
     return new_user
 
 
@@ -43,8 +50,10 @@ async def login(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
     session: SessionDep
 ) -> Token:
+    logger.info(f"Try to authenticate User: {form_data.username}")
     user = authenticate_user(form_data.username, form_data.password, session)
     if not user:
+        logger.warning("Authentication error: Incorrect username or password")
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password",
@@ -56,4 +65,5 @@ async def login(
         data={"sub": user.emailAddress}, expires_delta=access_token_expires
     )
 
+    logger.info(f"Authentication was successful")
     return Token(access_token=access_token, token_type="bearer")
