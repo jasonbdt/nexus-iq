53da9dec1eb432ac18fe1c299387865caf06aa00
jasonbdt <ja.bladt@gmail.com>
2026-02-03 09:09:45 +0100

    refactor: Remove unnecessary import

--- app/internal/services/vector_store.py
+++ app/internal/services/vector_store.py
@@ -3,7 +3,7 @@ import os
 import re
 import uuid
 
-from qdrant_client import AsyncQdrantClient, QdrantClient
+from qdrant_client import AsyncQdrantClient
 from qdrant_client.models import (
     Distance,
     VectorParams,
