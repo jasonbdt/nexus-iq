964368b6e6d78322fb4e5fae77f0e3c2a7ff3109
jasonbdt <ja.bladt@gmail.com>
2026-03-28 23:31:28 +0100

    refactor: Switch from blocking syncronous redis client to asyncronous non-blocking redis client

--- app/internal/redis.py
+++ app/internal/redis.py
@@ -4,30 +4,31 @@ import os
 import asyncio
 
 from fastapi import Depends
-from redis import Redis
 
-_lock = asyncio.Lock()
-_STATE: Redis | None = None
+import redis.asyncio as redis
 
-
-async def init_redis() -> Redis:
-    async with _lock:
-        global _STATE
-        _STATE = Redis(host=os.getenv("REDIS_HOST"), decode_responses=True)
-        return _STATE
+_STATE: redis.Redis | None = None
 
 
-def get_redis() -> Redis:
+
+async def init_redis() -> redis.Redis:
+    global _STATE
+    _STATE = redis.Redis(host=os.getenv("REDIS_HOST"))
+    return _STATE
+
+
+def get_redis() -> redis.Redis:
     if _STATE is None:
         raise RuntimeError("Redis not initialized. FastAPI lifespan didn't run?")
     return _STATE
 
 
 async def close_redis() -> None:
-    async with _lock:
-        global _STATE
-        if _STATE is not None:
-            _STATE.close()
-        _STATE = None
+    global _STATE
+    if _STATE is not None:
+        await _STATE.aclose()
+    _STATE = None
 
-RedisDep = Annotated[Redis, Depends(get_redis)]
+RedisDep = Annotated[redis.Redis, Depends(get_redis)]
+
+
